13d079d40a817eec5fbf85b9b5e8f73cad722714
Salohkin <54613748+Salohkin@users.noreply.github.com>
2023-12-22 11:40:46 +1100

    Initial commit

---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ChaseThemes
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "ChaseThemes";
+        public const string PLUGIN_NAME = "ChaseThemes";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
